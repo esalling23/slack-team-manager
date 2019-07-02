e381047be8da878258a4e0c54a82e5a9ec0f74ce
Glitch (ga-manager) <none>
2019-07-02 12:22:20 -0400

    Add slash command to search through available links

--- components/routes/incoming_webhooks.js
+++ components/routes/incoming_webhooks.js
@@ -12,6 +12,11 @@ module.exports = function (webserver, controller) {
     controller.handleWebhookPayload(req, res)
   })
 
+  // create all channels for a new cohort
+  // running in slack `/new-cohort 04 sei` will create:
+  // sei-bos-04-forum, sei-bos-04-planning, sei-bos-04-code, sei-bos-04-etc,
+  // sei-bos-04-links, sei-bos-04-projects, sei-bos-04-outcomes
+  // and invites the person who runs the command
   webserver.post('/new-cohort', function (req, res) {
     const cohort = req.body.text.split(' ')[0]
     const immersive = req.body.text.split(' ')[1]
@@ -60,4 +65,35 @@ module.exports = function (webserver, controller) {
       })
       .catch(console.error)
   })
+
+  // Search for relevant links
+  webserver.post('/quick-links', function (req, res) {
+    const category = req.body.text.split(' ')[0]
+    // const responseUrl = req.body.response_url
+
+    if (!category || !controller.searchLinks[category]) {
+      res.status(201).json({
+        response_type: 'ephemeral',
+        text: `Make sure to include a category like \`/quick-links orientation\`\n\nAvailable Categories: ${Object.keys(controller.searchLinks).join(', ')}`
+      })
+    } else {
+      let responseBody = `*Quick Links for ${category}:*\n`
+      controller.searchLinks[category].forEach(link => {
+        const name = link.match(/\[(.*?)\]/)
+        const url = link.match(/\((.*?)\)/)
+        responseBody += `\n <${url}|${name}>`
+      })
+
+      res.status(201).json({
+        response_type: 'ephemeral',
+        text: responseBody
+      })
+    }
+  })
+
+  // TO DO:
+  // look up missing assignments
+  // look up assigned homeworks so far
+  // look up upcoming lessons
+  // look up attendance standing?
 }
---
+++ skills/links.js
@@ -0,0 +1,31 @@
+module.exports = controller => {
+  controller.searchLinks = {
+    orientation: [
+      '[Program Roadmap](https://git.generalassemb.ly/ga-wdi-boston/orientation/blob/master/docs/program_roadmap.md)',
+      '[Daily Schedule](https://git.generalassemb.ly/ga-wdi-boston/orientation/blob/master/docs/example_day.md)',
+      '[Expectations](https://git.generalassemb.ly/ga-wdi-boston/orientation/blob/master/docs/expectations.md)',
+      '[Policies](https://git.generalassemb.ly/ga-wdi-boston/orientation/blob/master/docs/policies.md)',
+      '[Culture Code](https://git.generalassemb.ly/ga-wdi-boston/orientation/blob/master/docs/culture.md)'
+    ],
+    installfest: [
+      '[Main Repository](https://git.generalassemb.ly/ga-wdi-boston/installfest)',
+      '[Github and Initial Setup](https://git.generalassemb.ly/ga-wdi-boston/installfest/blob/master/github.md)',
+      '[Git](https://git.generalassemb.ly/ga-wdi-boston/installfest/blob/master/git.md)',
+      '[Bash Configuration](https://git.generalassemb.ly/ga-wdi-boston/installfest/blob/master/bash.md)',
+      '[Command Line Tools - Xcode (macOS only)](https://git.generalassemb.ly/ga-wdi-boston/installfest/blob/master/command_line_tools.md)',
+      '[Homebrew](https://git.generalassemb.ly/ga-wdi-boston/installfest/blob/master/homebrew.md)',
+      '[Node](https://git.generalassemb.ly/ga-wdi-boston/installfest/blob/master/node.md)',
+      '[Ruby](https://git.generalassemb.ly/ga-wdi-boston/installfest/blob/master/ruby.md)',
+      '[PostgreSQL](https://git.generalassemb.ly/ga-wdi-boston/installfest/blob/master/postgres.md)',
+      '[MongoDB](https://git.generalassemb.ly/ga-wdi-boston/installfest/blob/master/mongodb.md)',
+      '[Atom](https://git.generalassemb.ly/ga-wdi-boston/installfest/blob/master/atom.md)',
+      '[Chrome](https://git.generalassemb.ly/ga-wdi-boston/installfest/blob/master/chrome.md)'
+    ],
+    projects: [
+      '[Game Project](https://git.generalassemb.ly/ga-wdi-boston/game-project)',
+      '[Full Stack Project](https://git.generalassemb.ly/ga-wdi-boston/full-stack-project)',
+      '[Team Project](https://git.generalassemb.ly/ga-wdi-boston/team-project)',
+      '[Capstone Project](https://git.generalassemb.ly/ga-wdi-boston/capstone-project)'
+    ]
+  }
+}
