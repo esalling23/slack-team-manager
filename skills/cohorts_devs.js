1d49719bf6fecf1153626cf710cd58cc6f661245
Glitch (ga-manager) <none>
2019-09-27 15:09:07 -0400

    update current cohorts

--- skills/cohorts_devs.js
+++ skills/cohorts_devs.js
@@ -1,5 +1,5 @@
 module.exports = controller => {
-  controller.activeCohorts = ['03', '02']
+  controller.activeCohorts = ['04', '05']
   controller.developers = {
     '02': require('../json/bos_02.json'),
     '03': require('../json/bos_03.json')
